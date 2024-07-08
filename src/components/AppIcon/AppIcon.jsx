18f4eec0e3c8ebd53758da8a3d987855202caad1
Cristopher Bechtholdt <cristopherbechtholdt@gmail.com>
2024-07-07 22:26:26 -0300

    Add app icon component

---
+++ src/components/AppIcon/AppIcon.jsx
@@ -0,0 +1,12 @@
+import './AppIcon.css'
+
+const AppIcon = () => {
+  return (
+    <div className='app-icon-container'>
+        <div className='app-icon-roulette' />
+        <img className='app-icon' src="/PreguntadosIconoCara.png" />
+    </div>
+  )
+}
+
+export default AppIcon
