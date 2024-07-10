8b888f40838a3d390c1e2424053bbcee00f94227
Cristopher Bechtholdt <cristopherbechtholdt@gmail.com>
2024-07-10 19:28:34 -0300

    Add redirect for unknow route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Game from './pages/Game/Game'
 import History from './pages/History/History'
 import Home from './pages/Home/Home'
@@ -9,6 +9,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/game' element={<Game />} />
       <Route path='/history' element={<History />} />
+      <Route path='*' element={<Navigate to={"/"} />} />
     </Routes>
   )
 }
